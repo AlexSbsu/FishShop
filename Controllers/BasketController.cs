bb148f20743f031db9f34bf8af18711b9b97f805
COMP\1 <bsu_alexs@mail.ru>
2024-02-22 09:05:48 +0300

    PRE1:Selected prods to controller as array

---
+++ Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Fish_Shop
+{
+    public class BasketController : Controller
+    {
+        string? _basketProdList;
+        //[HttpPost]        
+        public IActionResult Index(string[] basketprodlist)
+        {
+
+            //Cookies
+            /*
+            string? prodsTobuycookie;
+            if (Request.Cookies.ContainsKey("prodsTobuycookie"))
+                prodsTobuycookie = Request.Cookies["prodsTobuycookie"];
+            else
+            {
+                prodsTobuycookie = "No prodsTobuycookie cookies found";
+                Response.Cookies.Append("prodsTobuycookie","empty");
+                prodsTobuycookie = Request.Cookies["prodsTobuycookie"];
+            } 
+            Console.WriteLine("prodsTobuycookie = " + Request.Cookies["prodsTobuycookie"]);
+            Console.WriteLine("All Cookies =" + Request.Cookies.ToString());
+            Console.WriteLine("Request.Cookies.Count().ToString(); =" + Request.Cookies.Count().ToString());
+            Console.WriteLine("Response.Cookies.Count().ToString(); =" + Response.Cookies.ToString());
+            */
+
+            if (basketprodlist == null) basketprodlist[0] = "emty";
+
+            Console.WriteLine("basketProdList = " + basketprodlist);
+            
+            return View(basketprodlist);
+        }
+
+        //[HttpGet]
+        public IActionResult Index2()
+        {            
+            Console.WriteLine("basketProdList = " + _basketProdList);
+
+            return View("Index", _basketProdList);
+        }
+
+
+    }
+}
---
+++ Models/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fish_Shop
+{
+    public class BasketItem
+    {
+        [Key]
+        public string Id { get; set; }
+        public string CartId { get; set; }
+        public int Amount { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
