e5423559cdb0a4e2cbea8fa30e0454fb3a530573
COMP\1 <bsu_alexs@mail.ru>
2024-03-01 06:55:06 +0300

    PRE3: Basket view - send order data to controller(amount+TotalCost+products) -> NEXT: create Order object basing on received data from frontend

---
+++ Models/FormOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fish_Shop
+{
+    public class FormOrder
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Comment { get; set; }
+
+        [Required]
+        public decimal TotalCost { get; set; }
+        
+        [Required]
+        public List<FormBasketItem> BasketItems { get; set; }
+    }
+}
--- obj/Debug/net8.0/Fish_Shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fish_Shop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fish_Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb148f20743f031db9f34bf8af18711b9b97f805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52ca584bb4f733e8622028c71c1b2491ffba06a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fish_Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fish_Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
